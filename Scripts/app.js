9606aedd64d26e420f66b3d2917276e1ce38aa17
MeerZaheen <MeerZ@Meers-MacBook-Pro.local>
2016-05-17 10:54:24 -0400

    added extra ! to "its alive"

--- Scripts/app.js
+++ Scripts/app.js
@@ -11,7 +11,7 @@ console.log("App Started...");
 // create a reference to a p element with an id of "firstParagraph"
 var firstParagraph = document.getElementById("firstParagraph");
 
-firstParagraph.textContent = "It's alive!!";
+firstParagraph.textContent = "It's alive!!!";
     
     
 })();

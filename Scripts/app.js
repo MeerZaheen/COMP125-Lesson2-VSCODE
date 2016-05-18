40317b0cce4c75a68a4c6cbdd065727a494a1ca6
MeerZaheen <MeerZ@Meers-MacBook-Pro.local>
2016-05-18 13:48:59 -0400

    modified app.js

--- Scripts/app.js
+++ Scripts/app.js
@@ -1,5 +1,5 @@
 /* main JavaScript file */
-
+// global scope- anything on global scope will give access to the visitor to edit and change
 // IIFE - Immediately Invoked Function Expression
 (function(){
     "use strict";
@@ -8,10 +8,38 @@
 // code goes in here
 console.log("App Started...");
 
-// create a reference to a p element with an id of "firstParagraph"
+
+
+// delare named function
+function writeToFirstParagraph() {
+    // for debugging only
+    console.log("Executing writeToFirstParagraph");
+    // create a reference to a p element with an id of "firstParagraph"
 var firstParagraph = document.getElementById("firstParagraph");
 
 firstParagraph.textContent = "It's alive!!!";
+    
+}
+
+// execute function
+
+/*
+// declare a variable that points to anonymous function
+var writeToFirstParagraph = function writeToFirstParagraph() {
+    // for debugging only
+    console.log("Executing writeToFirstParagraph");
+    // create a reference to a p element with an id of "firstParagraph"
+    var firstParagraph = document.getElementById("firstParagraph");
+
+    firstParagraph.textContent = "It's alive!!!";
+    
+}
+*/
+
+// execute function
+writeToFirstParagraph();
+
+// console.log(firstParagraph);
     
     
 })();

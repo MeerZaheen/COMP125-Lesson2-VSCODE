e0d6af20da203bb5829584571d5187110691c0a1
MeerZaheen <MeerZ@Meers-MacBook-Pro.local>
2016-05-18 14:29:03 -0400

    added event handler * not complete

--- Scripts/app.js
+++ Scripts/app.js
@@ -3,22 +3,52 @@
 // IIFE - Immediately Invoked Function Expression
 (function(){
     "use strict";
+    var firstParagraph = document.getElementById("firstParagraph");
 
+    // event listener click
+    firstParagraph.addEventListener("click", firstParagraphClicked);
+        // event listener hover 
+    firstParagraph.addEventListener("hover", firstParagraphWasHovered);
 
-// code goes in here
-console.log("App Started...");
+    var firstParagraphIsClicked = false;
+    var firstParagraphWasHovered = false;
 
+    // code goes in here
+    console.log("App Started...");
 
+    var myFirstParaGraph = "This is the first line of my first paragraph." + " This is the second line of my first paragraph.";
 
-// delare named function
-function writeToFirstParagraph() {
-    // for debugging only
-    console.log("Executing writeToFirstParagraph");
-    // create a reference to a p element with an id of "firstParagraph"
-var firstParagraph = document.getElementById("firstParagraph");
+    // delare named function
+    function writeToFirstParagraph() {
+        // for debugging only
+        console.log("Executing writeToFirstParagraph");
+        // create a reference to a p element with an id of "firstParagraph"
 
-firstParagraph.textContent = "It's alive!!!";
+        firstParagraph.textContent = myFirstParaGraph;
     
+}
+    // event handler for firstParagraphClicked
+    function firstParagraphClicked(){
+        
+        if(firstParagraphIsClicked){
+            firstParagraph.style.color = "#000000";
+            firstParagraphIsClicked = false;
+        }else {
+            firstParagraph.style.color = "red";
+            firstParagraphIsClicked = true;
+        }
+}
+
+    // event handler for firstParagraphClicked hover
+    function firstParagraphHover(){
+        
+        if(firstParagraphWasHovered){
+            firstParagraph.style.color = "#000000";
+            firstParagraphWasHovered = false;
+        }else {
+            firstParagraph.style.color = "red";
+            firstParagraphWasHovered = true;
+        }
 }
 
 // execute function
